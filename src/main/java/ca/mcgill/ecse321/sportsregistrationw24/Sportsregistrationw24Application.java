b06f84693df03e970902df1f72ab15ecaf9926b3
Ray <raydatray@gmail.com>
2024-02-12 22:24:08 -0500

    Initial Build Config

---
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/Sportsregistrationw24Application.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.sportsregistrationw24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sportsregistrationw24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sportsregistrationw24Application.class, args);
+	}
+
+}
