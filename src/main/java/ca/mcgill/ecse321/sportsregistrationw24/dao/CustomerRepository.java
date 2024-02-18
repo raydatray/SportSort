86c7c954d5c1ad0a0b243205989f3bd023fceac1
Colin Xiong <xiong.colin899@gmail.com>
2024-02-18 16:02:36 -0500

    #26 updated crud methods

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/CustomerRepository.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/CustomerRepository.java
@@ -3,12 +3,17 @@ package ca.mcgill.ecse321.sportsregistrationw24.dao;
 import ca.mcgill.ecse321.sportsregistrationw24.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
     Customer findCustomerById(Integer id);
-    boolean isRegisteredForClass(Integer classId);
-    boolean registerForClass(Integer classId);
-    boolean registerForSession(Integer sessionId);
-    boolean cancelClass(Integer classId);
+    Optional<Customer> findByEmail(String email);
+
+    // Check if a customer with a specific email exists
+    boolean existsByEmail(String email);
+
+    // Delete a customer by email
+    void deleteByEmail(String email);
     }
 
--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/RegistrationRepository.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/RegistrationRepository.java
@@ -4,6 +4,11 @@ import ca.mcgill.ecse321.sportsregistrationw24.model.Registration;
 import ca.mcgill.ecse321.sportsregistrationw24.model.keys.RegistrationId;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RegistrationRepository extends CrudRepository<Registration, Integer> {
     Registration getRegistrationById(RegistrationId id);
+    List<Registration> findByCustomerId(Integer customerId);
+    List<Registration> findBySportSessionId(Integer sportSessionId);
+    boolean existsByRegistrationById(RegistrationId id);
 }
