8d5b0ccb0156013dcb146a33d65673dee9717c8a
Colin Xiong <xiong.colin899@gmail.com>
2024-02-18 16:02:36 -0500

    #26 updated crud methods

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/SportSessionRepository.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/SportSessionRepository.java
@@ -3,6 +3,12 @@ package ca.mcgill.ecse321.sportsregistrationw24.dao;
 import ca.mcgill.ecse321.sportsregistrationw24.model.SportSession;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface SportSessionRepository extends CrudRepository<SportSession, Integer> {
     SportSession findSessionById(Integer classId);
+    List<SportSession> findBySessionType(SportSession.SessionType sessionType);
+    List<SportSession> findByDate(Date date);
+    List<SportSession> findByFloorNumberAndRoomNumber(Integer floorNumber, Integer roomNumber);
 }
