f4599788bff2a0b24b0448fa1dd02adb51b96d7e
Houman <veryhouman@gmail.com>
2024-02-18 17:17:33 -0500

    #26 Update Owner and Staff method repository

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/StaffRepository.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/StaffRepository.java
@@ -3,5 +3,8 @@ package ca.mcgill.ecse321.sportsregistrationw24.dao;
 import ca.mcgill.ecse321.sportsregistrationw24.model.Staff;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface StaffRepository extends CrudRepository<Staff, Integer> {
+    Optional<Staff> getStaffByEmail(String email);
 }
