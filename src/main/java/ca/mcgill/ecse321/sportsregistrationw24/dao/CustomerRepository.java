623356279faf6faadabd6093d8737c57a9b487c7
Houman <veryhouman@gmail.com>
2024-02-18 14:20:28 -0500

    #26 Created methods for Customer and Instructor repositories

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/CustomerRepository.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/CustomerRepository.java
@@ -1,9 +1,15 @@
 package ca.mcgill.ecse321.sportsregistrationw24.dao;
 
 import ca.mcgill.ecse321.sportsregistrationw24.model.Customer;
+import ca.mcgill.ecse321.sportsregistrationw24.model.SportClass;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
     Customer findCustomerById(Integer id);
-}
+    boolean isRegisteredForClass(Integer classId);
+    boolean registerForClass(Integer classId);
+    boolean registerForSession(Integer sessionId);
+    boolean cancelClass(Integer classId);
+    }
+
