1b7fd0f7e2b963991e76d842874d44d3a90efa10
Houman <veryhouman@gmail.com>
2024-02-18 15:02:04 -0500

    #26 Created methods Payment and Registration repositories

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/RegistrationRepository.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/RegistrationRepository.java
@@ -1,8 +1,9 @@
 package ca.mcgill.ecse321.sportsregistrationw24.dao;
 
 import ca.mcgill.ecse321.sportsregistrationw24.model.Registration;
+import ca.mcgill.ecse321.sportsregistrationw24.model.keys.RegistrationId;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RegistrationRepository extends CrudRepository<Registration, Integer> {
-
+    Registration getRegistrationById(RegistrationId id);
 }
