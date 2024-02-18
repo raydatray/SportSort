11ead06c53957ca4552ac82a6f9c82370284883f
Houman <veryhouman@gmail.com>
2024-02-18 14:54:57 -0500

    #26 Created methods for Owner, Center, Class, User and Session repositories

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/SportSessionRepository.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/SportSessionRepository.java
@@ -4,4 +4,5 @@ import ca.mcgill.ecse321.sportsregistrationw24.model.SportSession;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SportSessionRepository extends CrudRepository<SportSession, Integer> {
+    SportSession findSessionById(Integer classId);
 }
