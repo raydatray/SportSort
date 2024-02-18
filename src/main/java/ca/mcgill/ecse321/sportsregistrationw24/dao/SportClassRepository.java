d5be07bd93f1b53a7f6fbfa2484f7de08a743807
Houman <veryhouman@gmail.com>
2024-02-18 14:33:38 -0500

    #26 Created methods for Owner and Sport Class repositories

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/SportClassRepository.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/SportClassRepository.java
@@ -4,5 +4,7 @@ import ca.mcgill.ecse321.sportsregistrationw24.model.SportClass;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SportClassRepository extends CrudRepository<SportClass, Integer> {
+    Integer getFloorNumber(Integer classId);
+    Integer getRoomNumber(Integer classId);
 }
 
