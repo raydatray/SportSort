73da63f304cdeb773d905c0f44f8190e0066f176
Houman <veryhouman@gmail.com>
2024-02-18 14:33:38 -0500

    #26 Created methods for Owner and Sport Class repositories

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/OwnerRepository.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dao/OwnerRepository.java
@@ -1,7 +1,19 @@
 package ca.mcgill.ecse321.sportsregistrationw24.dao;
 
+import ca.mcgill.ecse321.sportsregistrationw24.model.Instructor;
 import ca.mcgill.ecse321.sportsregistrationw24.model.Owner;
+import ca.mcgill.ecse321.sportsregistrationw24.model.SportSession;
 import org.springframework.data.repository.CrudRepository;
 
+import java.sql.Date;
+import java.sql.Time;
+
 public interface OwnerRepository extends CrudRepository<Owner, Integer> {
+    Instructor createInstructor(Integer instructorId, String email, String password);
+    boolean deleteInstructor(Integer instructorId);
+    boolean modifySportCenter(Time openingHour, Time closingHour);
+    boolean modifySession(Integer sessionId, Date date, SportSession.SessionType sessionType, Time startTime, Time endTime, Integer floorNumber, Integer roomNumber);
+    boolean approveClass(Integer classId);
+    boolean hasPendingClasses();
+
 }
