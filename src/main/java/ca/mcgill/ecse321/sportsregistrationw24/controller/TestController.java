48675439bcf47d54d1edeb7813d440754c5b1f7a
Alexander Liu <alexander_l_22@hotmail.com>
2024-03-08 18:22:57 -0500

    Added relevant folders and placeholder files

---
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/controller/TestController.java
@@ -0,0 +1,6 @@
+package ca.mcgill.ecse321.sportsregistrationw24.controller;
+
+// DELETE THIS CLASS... IT IS JUST A PLACEHOLDER
+
+public class TestController {
+}
---
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dto/TestDTO.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.sportsregistrationw24.dto;
+
+// DELETE THIS CLASS... IT IS JUST A PLACEHOLDER
+public class TestDTO {
+}
