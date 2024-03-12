18113ff62567617d1cbe574ab64a9eca51e93c7b
Alexander Liu <alexander_l_22@hotmail.com>
2024-03-11 21:11:58 -0400

    Implemented service methods for registration class.

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/controller/RegistrationRestController.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/controller/RegistrationRestController.java
@@ -1,13 +1,46 @@
 package ca.mcgill.ecse321.sportsregistrationw24.controller;
 
-import ca.mcgill.ecse321.sportsregistrationw24.service.RegistrationService;
+import ca.mcgill.ecse321.sportsregistrationw24.dto.RegistrationDto;
+import ca.mcgill.ecse321.sportsregistrationw24.model.Registration;
+import ca.mcgill.ecse321.sportsregistrationw24.service.RegistrationCreationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class RegistrationRestController {
     @Autowired
-    private RegistrationService service;
+    private RegistrationCreationService service;
+
+    @PostMapping(value = {"/registrations/create", "/registrations/create/"})
+    public RegistrationDto createRegistration(@RequestBody RegistrationDto registrationDto) {
+        Registration registration = service.createRegistration();
+        return convertToDto(registration);
+    }
+
+    // NOTE: CHANGE FROM ID TO SMTH ELSE MAYBE
+    @GetMapping(value = {"/registrations/{id}", "/registrations/{id}/"})
+    public Registration getRegistration() {
+        return new Registration();
+    }
+
+    @GetMapping()
+    public List<RegistrationDto> getAllRegistrations() {
+        List<RegistrationDto> registrationDtos = new ArrayList<>();
+        for (Registration registration : service.getAllRegistrations()) {
+            registrationDtos.add(convertToDto(registration));
+        }
+        return registrationDtos;
+    }
+
+    private RegistrationDto convertToDto(Registration registration) {
+        if (registration == null) {
+            throw new IllegalArgumentException("There is no such registration!");
+        }
+        return new RegistrationDto(registration.getCourseOffering().getId(), registration.getCustomerAccount().getEmail(), registration.getRegisteredDate());
+    }
 }
--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/service/RegistrationService.java
+++ src/main/java/ca/mcgill/ecse321/sportsregistrationw24/service/RegistrationService.java
@@ -1,11 +1,107 @@
 package ca.mcgill.ecse321.sportsregistrationw24.service;
 
-import ca.mcgill.ecse321.sportsregistrationw24.dao.RegistrationRepository;
+import ca.mcgill.ecse321.sportsregistrationw24.dao.*;
+import ca.mcgill.ecse321.sportsregistrationw24.model.*;
+import ca.mcgill.ecse321.sportsregistrationw24.utilities.Utilities;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.util.Optional;
+import java.util.*;
+
 @Service
 public class RegistrationService {
-    @Autowired
-    private RegistrationRepository registrationRepository;
+  @Autowired
+  private RegistrationRepository registrationRepository;
+
+  @Autowired
+  private CustomerAccountRepository customerAccountRepository;
+
+  @Autowired
+  private CourseOfferingRepository courseOfferingRepository;
+
+  @Autowired
+  private CourseTypeRepository courseTypeRepository;
+
+  @Autowired
+  private InstructorAccountRepository instructorAccountRepository;
+
+  @Transactional
+  public Registration createRegistration(String courseTypeName, String instructorEmail, Date courseStartDate,
+                                         Date courseEndDate, String userAccountEmail, Date registrationDate) {
+    Registration newRegistration = new Registration();
+
+    Optional<CustomerAccount> wrappedCustomer = customerAccountRepository.findByEmail(userAccountEmail);
+    CustomerAccount customer = wrappedCustomer.orElseThrow(() -> new IllegalArgumentException("No customer account was found with the provided email!"));
+
+    Optional<CourseType> wrappedCourseType = courseTypeRepository.findByCourseName(courseTypeName);
+    CourseType courseType = wrappedCourseType.orElseThrow(() -> new IllegalArgumentException("No course type was found with the provided name!"));
+
+    Optional<InstructorAccount> wrappedInstructorAccount = instructorAccountRepository.findByEmail(instructorEmail);
+    InstructorAccount instructorAccount = wrappedInstructorAccount.orElseThrow(() -> new IllegalArgumentException("No instructor account was found with the provided email!"));
+
+    CourseOffering courseOffering = null;
+
+    for (CourseOffering c : courseOfferingRepository.findAll()) {
+      if (c.getCourseType().getCourseName().equals(courseType.getCourseName())
+              && c.getInstructorAccount().getEmail().equals(instructorAccount.getEmail())
+              && c.getStartDate().compareTo(courseStartDate) == 0
+              && c.getEndDate().compareTo(courseEndDate) == 0) {
+        courseOffering = c;
+      }
+    }
+
+    if (courseOffering == null) {
+      throw new IllegalArgumentException("No course offering was found with the provided information!");
+    }
+
+    newRegistration.setCustomerAccount(customer);
+    newRegistration.setCourseOffering(courseOffering);
+    newRegistration.setRegisteredDate(registrationDate);
+
+    registrationRepository.save(newRegistration);
+
+    return newRegistration;
+  }
+
+  // NOTE: IMPLEMENT THIS
+  @Transactional
+  public Registration getRegistration(String courseTypeName, String instructorEmail, Date courseStartDate,
+                                      Date courseEndDate, String userAccountEmail, Date registrationDate) {
+      Registration registration = null;
+
+      for (Registration r : registrationRepository.findAll()) {
+        if (r.getCourseOffering().getCourseType().getCourseName().equals(courseTypeName)
+                && r.getCourseOffering().getInstructorAccount().getEmail().equals(instructorEmail)
+                && r.getCourseOffering().getStartDate().compareTo(courseStartDate) == 0
+                && r.getCourseOffering().getEndDate().compareTo(courseEndDate) == 0
+                && r.getCustomerAccount().getEmail().equals(userAccountEmail)
+                && r.getRegisteredDate().compareTo(registrationDate) == 0) {
+          registration = r;
+        }
+      }
+      // If a registration is matched, then return an object else null
+      return registration;
+  }
+
+  @Transactional
+  public void deleteRegistration(String courseTypeName, String instructorEmail, Date courseStartDate,
+                                 Date courseEndDate, String userAccountEmail, Date registrationDate) {
+    Registration registration = getRegistration(courseTypeName, instructorEmail, courseStartDate,
+            courseEndDate, userAccountEmail, registrationDate);
+
+    if (registration == null) {
+      throw new IllegalArgumentException("Registration does not exist!");
+    }
+
+    registrationRepository.delete(registration);
+  }
+
+  @Transactional
+  public List<Registration> getAllRegistrations() {
+    Utilities utilities = new Utilities();
+    return utilities.iterableToArrayList(registrationRepository.findAll());
+  }
 }
