0aa3c2ee72f4535d3297e99b456791f27ef237fa
Ray <raydatray@gmail.com>
2024-03-18 00:09:31 -0400

    Removed test files and unused files

--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/dto/TestDTO.java
+++
@@ -1,5 +0,0 @@
-package ca.mcgill.ecse321.sportsregistrationw24.dto;
-
-// DELETE THIS CLASS... IT IS JUST A PLACEHOLDER
-public class TestDTO {
-}
--- src/main/java/ca/mcgill/ecse321/sportsregistrationw24/service/TestService.java
+++
@@ -1,5 +0,0 @@
-package ca.mcgill.ecse321.sportsregistrationw24.service;
-
-// DELETE THIS CLASS... IT IS JUST A PLACEHOLDER
-public class TestService {
-}
