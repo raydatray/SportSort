b6a0c5d1ed9f160aa91f2ae06720317854ae8ce9
Alexander Liu <alexander_l_22@hotmail.com>
2024-03-23 01:08:16 -0400

    Added template file for service test classes.

---
+++ src/test/java/ca/mcgill/ecse321/sportsregistrationw24/service/TemplateFile.java
@@ -0,0 +1,32 @@
+package ca.mcgill.ecse321.sportsregistrationw24.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+// Import anything else you need
+
+@ExtendWith(MockitoExtension.class)
+public class TemplateFile {
+  // Check tutorial notes on how to write the body of the test class...
+  // https://mcgill-ecse321-winter2024.github.io/tutorial-notes/#_implementing_unit_tests_for_service_class
+}
