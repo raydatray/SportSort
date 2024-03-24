42eb44977b22cad6c35c8f91e9404e58ebd03e63
Alexander Liu <alexander_l_22@hotmail.com>
2024-03-24 15:08:20 -0400

    Added template file for integration tests

---
+++ src/test/java/ca/mcgill/ecse321/sportsregistrationw24/integration/TemplateFile.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class TemplateFile {
+  // ADD YOUR TEST
+  // Check out https://mcgill-ecse321-winter2024.github.io/tutorial-notes/#_service_integration_testing_with_springs_testresttemplate
+}
