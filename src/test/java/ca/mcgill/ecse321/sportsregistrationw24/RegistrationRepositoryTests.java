19135d82423fa5910dca8a2ac0614123976ffa73
Joseph Ciaravella <joseph.ciaravella@gmail.com>
2024-03-25 02:42:49 -0400

    Fixed location of service tests and modified the service layer when it did not pass the tests. Also added a setID to paymentInfo model because there is no other way to access a PaymentInfo besides with its ID and there would have been no way to test if I could not search by ID. All service tests pass. Integration tests to be implemented.

--- src/test/java/ca/mcgill/ecse321/sportsregistrationw24/RegistrationRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/sportsregistrationw24/RegistrationRepositoryTests.java
@@ -46,141 +46,77 @@ import org.mockito.stubbing.Answer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class RegistrationRepositoryTests {
-  @Mock
-  private RegistrationRepository registrationDao;
+  @Autowired
+  private RegistrationRepository registrationRepository;
 
-  @Mock
-  private CourseOfferingRepository courseOfferingDao;
-/*
-  //do I need CourseType??
-  @Mock
-  private CourseTypeRepository courseTypeDao;
+  @Autowired
+  private CourseOfferingRepository courseOfferingRepository;
 
-  @Mock
-  private RoomRepository roomDao;
+  @Autowired
+  private CourseTypeRepository courseTypeRepository;
 
-  @Mock
-  private InstructorAccountRepository instructorAccountDao;
-*/
-  @Mock
-  private CustomerAccountRepository customerAccountDao;
+  @Autowired
+  private RoomRepository roomRepository;
 
-  @Mock
-  private PaymentInfoRepository paymentInfoDao;
+  @Autowired
+  private InstructorAccountRepository instructorAccountRepository;
 
+  @Autowired
+  private CustomerAccountRepository customerAccountRepository;
+
+  @Autowired
+  private PaymentInfoRepository paymentInfoRepository;
 
 
-  @InjectMocks
-  private RegistrationService registrationService;
-  /*
-  @InjectMocks
-  private CustomerAccountService
-  */
-  private static final Integer CUSTOMER_ID = 1;
-  private static final Integer COURSE_OFFERING_ID = 1;
-  private static final Date DATE = Date.valueOf("2024-01-02");
-  private static final Integer PAYMENT_INFO_ID = 1;
-  private static final Integer INSTRUCTOR_ID = 1;
-
-  @BeforeEach
-  public void setMockOutput() {
-    lenient().when(customerAccountDao.findByToken(anyString())).thenAnswer((InvocationOnMock invocation) -> {
-      if (invocation.getArgument(0).equals(CUSTOMER_ID)) {
-        CustomerAccount customerAccount = new CustomerAccount();
-        customerAccount.setName("John");
-        customerAccount.setEmail("john@gmail.com");
-        customerAccount.setPassword("HelloJohn7!");
-        return Optional.of(customerAccount);
-      } else {
-        return Optional.empty();
-      }
-    });
-    lenient().when(courseOfferingDao.findById(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
-      if (invocation.getArgument(0).equals(COURSE_OFFERING_ID)) {
-        CourseOffering courseOffering = new CourseOffering();
-        courseOffering.setId(COURSE_OFFERING_ID);
-        courseOffering.setStartDate(Date.valueOf("2024-01-01")); // Start date = Jan 1st, 2024
-        courseOffering.setEndDate(Date.valueOf("2024-02-01")); // End date = Feb 1st, 2024
-        return Optional.of(courseOffering);
-      } else {
-        return Optional.empty();
-      }
-    });
-    lenient().when(paymentInfoDao.findById(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
-      if (invocation.getArgument(0).equals(PAYMENT_INFO_ID)) {
-        PaymentInfo paymentInfo = new PaymentInfo();
-        // SET FIELDS
-        paymentInfo.setCustomerAccount(customerAccountDao.findById(CUSTOMER_ID).orElse(null));
-        return Optional.of(paymentInfo);
-      } else {
-        return Optional.empty();
-      }
-    });
-    // Whenever anything is saved, just return the parameter object
-    Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
-      return invocation.getArgument(0);
-    };
-    lenient().when(customerAccountDao.save(any(CustomerAccount.class))).thenAnswer(returnParameterAsAnswer);
-    lenient().when(courseOfferingDao.save(any(CourseOffering.class))).thenAnswer(returnParameterAsAnswer);
-    lenient().when(registrationDao.save(any(Registration.class))).thenAnswer(returnParameterAsAnswer);
-    /*
-    lenient().when(courseTypeDao.save(any(CourseType.class))).thenAnswer(returnParameterAsAnswer);
-    lenient().when(roomDao.save(any(Room.class))).thenAnswer(returnParameterAsAnswer);
-    lenient().when(instructorAccountDao.save(any(InstructorAccount.class))).thenAnswer(returnParameterAsAnswer);
-     */
-    lenient().when(paymentInfoDao.save(any(PaymentInfo.class))).thenAnswer(returnParameterAsAnswer);
-  }
   @BeforeEach
   @AfterEach
   public void clearDatabase() {
-      registrationDao.deleteAll();
-      courseOfferingDao.deleteAll();
-      paymentInfoDao.deleteAll();
-      customerAccountDao.deleteAll();
-      /*
-      courseTypeDao.deleteAll();
-      roomDao.deleteAll();
-      instructorAccountDao.deleteAll();
-       */
+      registrationRepository.deleteAll();
+      courseOfferingRepository.deleteAll();
+      paymentInfoRepository.deleteAll();
+      customerAccountRepository.deleteAll();
+      courseTypeRepository.deleteAll();
+      roomRepository.deleteAll();
+      instructorAccountRepository.deleteAll();
     }
-/*
+
   @Test
   public void testPersistAndLoadRegistration() {
       CourseType courseType = new CourseType("Cardio");
       courseType.setApproved(true);
-      courseTypeDao.save(courseType);
+      courseTypeRepository.save(courseType);
 
       Room testRoom = new Room("Pool", 10, 10, 10);
-      roomDao.save(testRoom);
+      roomRepository.save(testRoom);
 
       InstructorAccount testInstructor = new InstructorAccount("ray","raydatray@gmail.com", "password");
-      instructorAccountDao.save(testInstructor);
+      instructorAccountRepository.save(testInstructor);
 
       Date startDate = Date.valueOf("2024-02-18");
       Date endDate = Date.valueOf("2024-03-15");
       ArrayList<DayOfWeek> testDays = new ArrayList<>(List.of(DayOfWeek.MONDAY, DayOfWeek.FRIDAY));
 
       CourseOffering testOffering  = new CourseOffering(startDate, endDate, testDays, testRoom, courseType, testInstructor);
-      courseOfferingDao.save(testOffering);
+      courseOfferingRepository.save(testOffering);
 
       String testName = "joe";
       String testEmail = "joebama@gmail.com";
       String testPassword = "obama";
 
       CustomerAccount testCustomer = new CustomerAccount(testName, testEmail, testPassword);
-      customerAccountDao.save(testCustomer);
+      customerAccountRepository.save(testCustomer);
 
       PaymentInfo testPayment = new PaymentInfo(PaymentInfo.PaymentType.Credit, 12345, 123, 24, 6, testCustomer);
-      paymentInfoDao.save(testPayment);
+      paymentInfoRepository.save(testPayment);
 
       Date testDate = Date.valueOf("2024-03-04");
 
       Registration testRegistration = new Registration(testDate, testOffering, testCustomer, testPayment);
-      registrationDao.save(testRegistration);
+      registrationRepository.save(testRegistration);
 
-      Optional<List<Registration>> readRegistrations = registrationDao.findByCustomerAccount(testCustomer);
+      Optional<List<Registration>> readRegistrations = registrationRepository.findByCustomerAccount(testCustomer);
 
       List<Registration> testRegistrations;
 
@@ -208,331 +144,5 @@ public class RegistrationRepositoryTests {
       assertEquals(foundRegistration.getCustomerAccount().getId(), testCustomer.getId());
 
       assertEquals(testDate, foundRegistration.getRegisteredDate());
-  }
- */
-  /*-------- Tests for createRegistration() --------*/
-  @Test
-  public void testCreateRegistration() {
-    assertEquals(0, registrationService.getAllRegistrations().size());
-
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(COURSE_OFFERING_ID, CUSTOMER_ID, PAYMENT_INFO_ID, DATE);
-    } catch (IllegalArgumentException e) {
-      fail();
-    }
-
-    assertNotNull(registration);
-    assertEquals(COURSE_OFFERING_ID, registration.getCourseOffering().getId());
-    assertEquals(CUSTOMER_ID, registration.getCustomerAccount().getId());
-    assertEquals(PAYMENT_INFO_ID, registration.getPaymentInfo().getId());
-    assertEquals(DATE, registration.getRegisteredDate());
-  }
-
-  // can make this impossible to get wrong in frontend
-  @Test
-  public void testCreateRegistrationWithNullDate() {
-    Date registeredDate = null;
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(COURSE_OFFERING_ID, CUSTOMER_ID, PAYMENT_INFO_ID, registeredDate);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Must register on a date that is not null!", error);
-  }
-
-  public void testCreateRegistrationWithNullCourseOffering() {
-    CourseOffering courseOffering = null;
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(courseOffering.getId(), CUSTOMER_ID, PAYMENT_INFO_ID, DATE);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("No course offering was found with the provided information!", error);
-  }
-
-  public void testCreateRegistrationWithNullPaymentInfo() {
-    PaymentInfo paymentInfo = null;
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(COURSE_OFFERING_ID, CUSTOMER_ID, paymentInfo.getId(), DATE);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("No payment info was found with the provided information!", error);
-  }
-
-  public void testCreateRegistrationWithNullCustomerAccount() {
-    CustomerAccount customerAccount = null;
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(COURSE_OFFERING_ID, customerAccount.getId(), PAYMENT_INFO_ID, DATE);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("No customer account was found with the provided information!", error);
-  }
-
-  public void testCreateRegistrationWithInvalidCourseOffering() {
-    Integer courseOfferingID = 5;
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(courseOfferingID, CUSTOMER_ID, PAYMENT_INFO_ID, DATE);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("No course offering was found with the provided information!", error);
-  }
-
-  public void testCreateRegistrationWithInvalidCustomerAccount() {
-    Integer customerAccountID = 8;
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(COURSE_OFFERING_ID, customerAccountID, PAYMENT_INFO_ID, DATE);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("No customer account was found with the provided information!", error);
-  }
-
-  public void testCreateRegistrationWithInvalidPaymentInfo() {
-    Integer paymentinfoID = 6;
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(COURSE_OFFERING_ID, CUSTOMER_ID, paymentinfoID, DATE);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("No payment info was found with the provided information!", error);
-  }
-
-  // create with date after course offering end date
-  public void testCreateRegistrationWithDateAfterCourseOfferingEndDate() {
-    Date registeredDate = Date.valueOf("2024-03-01");
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(COURSE_OFFERING_ID, CUSTOMER_ID, PAYMENT_INFO_ID, registeredDate);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("You must register for a course offering at most one day before it starts!", error);
-  }
-  // create with date after course offering start date
-  public void testCreateRegistrationWithDateAfterCourseOfferingStartDate() {
-    Date registeredDate = Date.valueOf("2024-02-10");
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(COURSE_OFFERING_ID, CUSTOMER_ID, PAYMENT_INFO_ID, registeredDate);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("You must register for a course offering at most one day before it starts!", error);
-  }
-
-  public void testCreateRegistrationWithSameDateAsCourseOfferingStartDate() {
-    Date registeredDate = Date.valueOf("2024-02-01");
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.createRegistration(COURSE_OFFERING_ID, CUSTOMER_ID, PAYMENT_INFO_ID, registeredDate);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("You must register for a course offering at most one day before it starts!", error);
-  }
-
-
-
-  /*-------- Tests for getRegistration() --------*/
-  public void testGetRegistration() {
-    RegistrationId registrationID = new RegistrationId(CUSTOMER_ID, COURSE_OFFERING_ID);
-
-    Registration registration = null;
-    try {
-      registration = registrationService.getRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      fail();
-    }
-    assertNotNull(registration);
-    assertEquals(COURSE_OFFERING_ID, registration.getCourseOffering().getId());
-    assertEquals(CUSTOMER_ID, registration.getCustomerAccount().getId());
-    assertEquals(PAYMENT_INFO_ID, registration.getPaymentInfo().getId());
-    assertEquals(DATE, registration.getRegisteredDate());
-  }
-  public void testGetRegistrationWithNullRegistrationID() {
-    RegistrationId registrationID = null;
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.getRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
-  }
-
-  public void testGetRegistrationWithNullCustomerAccountID() {
-    Integer customerAccountID = null;
-    RegistrationId registrationID = new RegistrationId(customerAccountID, COURSE_OFFERING_ID);
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.getRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
-  }
-
-  public void testGetRegistrationWithNullCourseOfferingID() {
-    Integer courseOfferingID = null;
-    RegistrationId registrationID = new RegistrationId(CUSTOMER_ID, courseOfferingID);
-
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.getRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
-  }
-
-  public void testGetRegistrationWithInvalidCustomerAccountID() {
-    Integer customerAccountID = 19;
-    RegistrationId registrationID = new RegistrationId(customerAccountID, COURSE_OFFERING_ID);
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.getRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
-  }
-
-  public void testGetRegistrationWithInvalidCourseOfferingID() {
-    Integer courseOfferingID = 6;
-    RegistrationId registrationID = new RegistrationId(CUSTOMER_ID, courseOfferingID);
-
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registration = registrationService.getRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
-  }
-
-
-  /*-------- Tests for deleteRegistration() --------*/
-  public void testDeleteRegistration() {
-    RegistrationId registrationID = new RegistrationId(CUSTOMER_ID, COURSE_OFFERING_ID);
-
-    Registration registration = registrationService.getRegistration(registrationID);
-    try {
-      registrationService.deleteRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      fail();
-    }
-    // i think this is all i need
-    assertNull(registration);
-  }
-  public void testDeleteRegistrationWithNullRegistrationID() {
-    RegistrationId registrationID = null;
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registrationService.deleteRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
-  }
-
-  public void testDeleteRegistrationWithNullCustomerAccountID() {
-    Integer customerAccountID = null;
-    RegistrationId registrationID = new RegistrationId(customerAccountID, COURSE_OFFERING_ID);
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registrationService.deleteRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
-  }
-
-  public void testDeleteRegistrationWithNullCourseOfferingID() {
-    Integer courseOfferingID = null;
-    RegistrationId registrationID = new RegistrationId(CUSTOMER_ID, courseOfferingID);
-
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registrationService.deleteRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
-  }
-
-  public void testDeleteRegistrationWithInvalidCustomerAccountID() {
-    Integer customerAccountID = 19;
-    RegistrationId registrationID = new RegistrationId(customerAccountID, COURSE_OFFERING_ID);
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registrationService.deleteRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
-  }
-
-  public void testDeleteRegistrationWithInvalidCourseOfferingID() {
-    Integer courseOfferingID = 6;
-    RegistrationId registrationID = new RegistrationId(CUSTOMER_ID, courseOfferingID);
-
-
-    String error = null;
-    Registration registration = null;
-    try {
-      registrationService.deleteRegistration(registrationID);
-    } catch (IllegalArgumentException e) {
-      error = e.getMessage();
-    }
-    assertEquals("Registration does not exist!", error);
   }
 }
