adbe2364c3f3f4cd10f011364339ace1721a7ac6
Houman <hooazari@gmail.com>
2024-04-01 13:55:16 -0400

    Add one checkbox

---
+++ Frontend/src/components/Checkbox.svelte
@@ -0,0 +1,19 @@
+<script>
+  // Script logic (if necessary) goes here
+</script>
+
+<div class="form-control">
+  <label class="label cursor-pointer">
+    <span class="label-text">Remember me</span>
+    <input type="checkbox" checked="checked" class="checkbox" />
+  </label>
+</div>
+
+<style>
+  .form-control {
+    visibility: visible;
+    margin: auto;
+    margin-top: 150px;
+    margin-left: 100px;
+  }
+</style>
