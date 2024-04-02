822ec9ffd0939edbd5dacbd351c024913f998bf4
Alexander Liu <alexander_l_22@hotmail.com>
2024-04-02 12:37:22 -0400

    Updated UI styling for account creation page and login page

--- Frontend/src/lib/components/CreateAccountPage.svelte
+++ Frontend/src/lib/components/CreateAccountPage.svelte
@@ -4,8 +4,8 @@
     }
 </script>
 
-<div class="create-account-page" data-theme="nord">
-    <div class="create-account-form">
+<div class="create-account-page bg-base">
+    <div class="create-account-form bg-primary">
         <h1>
             CREATE ACCOUNT
         </h1>
@@ -25,7 +25,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
             <input type="password" class="grow" placeholder="Confirm password" />
         </label>
-        <button class="btn" on:click={createAccount}>Create Account</button>
+        <button class="btn bg-accent" on:click={createAccount}>Create Account</button>
         <p class="sign-in-footer">Already have an account? <a href="https://www.youtube.com/watch?v=-vjNAyL2JCQ" class="underline">Sign in</a></p>
     </div>
 </div>
@@ -40,7 +40,8 @@
     }
 
     button {
-        width: 100%;
+        margin-top: 25px;
+        width: 60%;
     }
 
     label {
--- Frontend/src/lib/components/LoginPage.svelte
+++ Frontend/src/lib/components/LoginPage.svelte
@@ -4,8 +4,8 @@
     }
 </script>
 
-<div class="login-page" data-theme="nord">
-    <div class="login-form">
+<div class="login-page bg-base">
+    <div class="login-form bg-primary">
         <h1>
             LOGIN
         </h1>
@@ -17,11 +17,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
             <input type="password" class="grow" placeholder="Password" />
         </label>
-        <label class="input input-bordered flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
-            <input type="password" class="grow" placeholder="Password" />
-            </label>
-        <button class="btn" on:click={login}>Login</button>
+        <button class="btn bg-accent" on:click={login}>Login</button>
         <p class="sign-up-footer">Don't have an account? <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="underline">Sign up</a></p>
     </div>
 </div>
@@ -36,7 +32,8 @@
     }
 
     button {
-        width: 100%;
+        width: 60%;
+        margin-top: 25px;
     }
 
     label {
