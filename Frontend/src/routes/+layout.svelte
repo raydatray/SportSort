52d4dcf8d5db0c296b077ad11800ce6e6bd61d38
Colin Xiong <xiong.colin899@gmail.com>
2024-04-05 17:28:26 -0400

    Added back functionality and final look

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -5,20 +5,22 @@
 </script>
 
 <div class="h-screen grid grid-cols-[240px_1fr] gap-x-1 p-2" data-theme="north">
-  <ul class="p-4 m-2 menu-horizontal md:menu-vertical bg-base-200 rounded-box gap-y-1">
-    <li class="flex-row justify-center p-2 m-1">
+  <nav class="menu justify-items-center">
+  <ul class="p-3.5 m-1 menu-horizontal md:menu-vertical bg-base-200 rounded-box gap-y-0.5">
+    <li class="flex-row justify-center">
       <img class = "max-h-100px" src={Logo} alt="Logo"/>
     </li>
-    <li class="h-0.5 spacer2 bg-base-300"></li> 
-    <li class = "gap-y-.5 m-2"><a class = "flex items-center space-x-2 p-0.2"> <IconHome /> Home </a></li>
-    <li class = "gap-y-.5 m-2"><a class = "flex items-center space-x-2 p-0.2"> <IconSchool /> Course Offerings </a></li>
-    <li class = "gap-y-.5 m-2"><a class = "flex items-center space-x-2 p-0.2"> <IconHistory /> Course History </a></li>
-    <li class = "gap-y-.5 m-2"><a class = "flex items-center space-x-2 p-0.2"> <IconTicket /> Registrations </a></li>
+    <li class="h-0.5 -ml-px w-full spacer2 bg-base-400 justify-center"></li> 
+    <li class = "m-1 "><a class = "flex items-center space-x-2 p-0.2"> <IconHome /> Home </a></li>
+    <li class = "m-1 "><a class = "flex items-center space-x-2 p-0.2"> <IconSchool /> Course Offerings </a></li>
+    <li class = "m-1 "><a class = "flex items-center space-x-2 p-0.2"> <IconHistory /> Course History </a></li>
+    <li class = "m-1 "><a class = "flex items-center space-x-2 p-0.2"> <IconTicket /> Registrations </a></li>
     <li class="spacer bg-base-200"></li> <!-- This spacer will now push the settings and logout to the bottom -->
-    <li class="h-0.5 spacer2 bg-base-300"></li> 
-    <li class = "gap-y-.5 m-2"><a class = "flex items-center space-x-2 p-0.2"> <IconSettings /> Account Settings </a></li>
-    <li class = "gap-y-.5 m-2"><a class = "flex items-center space-x-2 p-0.2"> <IconLogout /> Logout </a></li>
+    <li class="h-0.5 -ml-px w-full spacer2 bg-base-400 justify-center"></li> 
+    <li class = "m-1 "><a class = "flex items-center space-x-2 p-0.2"> <IconSettings /> Account Settings </a></li>
+    <li class = "m-1 "><a class = "flex items-center space-x-2 p-0.2"> <IconLogout /> Logout </a></li>
   </ul>
+</nav>
   <div class="content">
     <slot />
     <footer class="p-4 footer bg-base-300 text-base-content">
@@ -39,27 +41,6 @@
     width: 100vw; /* Use viewport width to ensure full width coverage */
   }
   
-  .menu {
-    background-color: rgba(255, 255, 255, 0.05);
-    height: 100vh;
-    overflow-y: auto;
-    margin: 0;
-    bottom: 0;
-    border-radius: 10px;
-  }
-
-  .logo-container {
-    display: flex;
-    justify-content: center;
-    margin: 1em 0;
-    margin-top: 8%;
-  }
-
-  .logo {
-    max-height: 100px;
-    width: auto;
-  }
-
   ul {
     display: flex;
     flex-direction: column;
