7a501967d6a2b681772a479fbca8fb20c08f861f
leon.song@mail.mcgil.ca <leonsongmtl@hotmail.com>
2024-04-07 16:58:54 -0400

    Added a page creation starting block

---
+++ Frontend/src/routes/CourseTypeApproval/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import ListInfiniteScroll from "$lib/components/ListInfiniteScroll.svelte";
+    import ListCourseTypeApproval from "$lib/components/ListCourseTypeApproval.svelte";
+</script>
+<div class="container">
+    <ListCourseTypeApproval />
+    <ListInfiniteScroll />
+</div>
+<style>
+
+</style>
